85ac97fe5c3d0a5ce7a2f07353ef5cedae03565e
Ayoku Gilbert <gilbertayoku3@gmail.com>
2025-04-03 14:27:36 +0100

    fix: correct CORS middleware usage and simplify event response structure

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(express.json())
 
-app.cors()
+app.use(cors());
 
 app.use(router);
 
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -10,7 +10,7 @@ router.get("/api/events", async (req, res) => {
         const getEvents = await Event.find();
         if((getEvents == [])) return res.status(404).send("No event found")
         
-        return res.status(200).send({ events: getEvents})
+        return res.status(200).send(getEvents)
     } catch (error) {
         console.log("Error getting Events: ", error);
         return res.status(500).send({ err_msg: error})
